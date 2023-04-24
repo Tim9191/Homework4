db7af232ff32b7e32e8d92a24b6b206641878f9f
Teymur <teymur_aliyev@hotmail.com>
2023-04-24 21:18:29 +0400

    Add project files.

---
+++ Homework4/Homework4.py
@@ -0,0 +1,45 @@
+# Task 22
+
+
+#n = int(input("Enter the number of elements of the first set: "))
+#m = int(input("Enter the number of elements of the second set: "))
+
+#set1 = set()
+#set2 = set()
+
+#print("Enter the elements of the first set:")
+#for i in range(n):
+#    set1.add(int(input()))
+
+#print("Enter elements of the second set:")
+#for i in range(m):
+#    set2.add(int(input()))
+
+#result_set = set1.intersection(set2)
+#result_list = sorted(list(result_set))
+
+#print("Elements that occur in both sets: ")
+#for num in result_list:
+#    print(num)
+
+
+
+# Task 24
+
+
+
+#with open('input.txt') as f_in, open('output.txt', 'w') as f_out:
+#    n = int(f_in.readline().strip())
+#    bushes = list(map(int, f_in.readline().split()))
+    
+#    max_harvest = 0
+#    for i in range(n - 2):
+#        harvest = bushes[i] + bushes[i+1] + bushes[i+2]
+#        if harvest > max_harvest:
+#            max_harvest = harvest
+    
+#    f_out.write(str(max_harvest))
+
+
+
+
